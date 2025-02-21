92867735eb85cdbc08089d4971efe185694146b2
Ekin Utku <ekinutkugulmez@gmail.com>
2025-02-21 13:48:09 +0300

    added page routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
-import './index.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import './index.css';
+import Realms from "./pages/Realms";
 
 function App() {
    return (
-      <div className="App"></div>
+      <Router>
+         <div className="App">
+            <Routes>
+               <Route path="/" element={<Realms />} />
+               <Route path="/realms" element={<Realms />} />
+            </Routes>
+         </div>
+      </Router>
    );
 }
 
