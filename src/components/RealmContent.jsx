7313174f3da3b55f937ed241934ba084c11a0a1e
Ekin Utku <ekinutkugulmez@gmail.com>
2025-03-04 12:50:20 +0300

    filter component added to realm page

---
+++ src/components/RealmContent.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const RealmContent = () => {
+   return (
+      <div className="w-fit flex justify-center items-center whitespace-nowrap bg-[#242323] text-light px-6 py-2.5 mx-2 rounded-xl cursor-pointer border border-transparent hover:border-[#999999] duration-300">
+         Karakterler
+      </div>
+   );
+};
+
+export default RealmContent;
